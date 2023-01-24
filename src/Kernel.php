5663149efc2e55eacf7c7b3918218659a75d386f
Hugo Nicolas <hugo.nicolas@les-tilleuls.coop>
2023-01-24 15:55:47 +0100

    fix: enable config from non symfony app

--- src/Kernel.php
+++ src/Kernel.php
@@ -15,11 +15,34 @@ namespace ApiPlatform\PDGBundle;
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    private function configureContainer(ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder): void
+    {
+        $configDir = $this->getConfigDir();
+
+        $container->import($configDir.'/{packages}/*.{php,yaml}');
+        $container->import($configDir.'/{packages}/'.$this->environment.'/*.{php,yaml}');
+
+        $pdgConfig = getenv('PDG_CONFIG');
+        if ($pdgConfig && is_file(getcwd(). DIRECTORY_SEPARATOR. $pdgConfig)) {
+            $container->import(getcwd().DIRECTORY_SEPARATOR.$pdgConfig);
+        }
+
+        if (is_file($configDir.'/services.yaml')) {
+            $container->import($configDir.'/services.yaml');
+            $container->import($configDir.'/{services}_'.$this->environment.'.yaml');
+        } else {
+            $container->import($configDir.'/{services}.php');
+        }
+    }
 
     public function getProjectDir(): string
     {
@@ -30,6 +53,7 @@ class Kernel extends BaseKernel
     {
         return [
             new FrameworkBundle(),
+            new ApiPlatformPDGBundle()
         ];
     }
 }
