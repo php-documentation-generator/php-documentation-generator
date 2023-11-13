e83b4af837da305c36230adf294dad4b1859313a
soyuka <soyuka@users.noreply.github.com>
2023-11-13 17:41:58 +0100

    use yaml

--- template/guides/guide.php
+++ template/guides/guide.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\Yaml\Yaml;
+
 /*
  * This file is part of the PHP Documentation Generator project
  *
@@ -10,53 +12,40 @@
  */
 
 return static function (array $headers, array $sections): string {
-
     $t = '';
-    $t .= implode('', $headers);
+    $stop = false;
+    foreach ($headers as $header) {
+        if (str_contains($header, '---')) {
+            if ($stop) {
+                continue;
+            }
 
-    foreach ($sections as $i => $section) {
-        $id = 'section-'.$i;
-        $texte = implode('', $section['text']).\PHP_EOL;
-        $code = implode('', $section['text']).\PHP_EOL;
-        $code = '';
-        if ($section['code'] ?? null) {
-            $code .= \PHP_EOL;
-            $code .= '```php'.\PHP_EOL;
-            $code .= implode('', $section['code']).\PHP_EOL;
-            $code .= '```'.\PHP_EOL;
-            $code .= \PHP_EOL;
+            $stop = true;
         }
 
-        $t .= <<<HTML
-<div class="grid grid-cols-1 py-4 group xl:grid-cols-2 pt-8">
-      <div class="flex-1 flex flex-row border-b-gray-300 dark:border-blue-dark xl:border-b-px border-dotted group-last:border-0 xl:mr-8 xl:pb-8 ">
-        <div class="relative w-full xl:pr-8">
-          <a
-            id="$id"
-            href="#${id}"
-            class="absolute right-full -translate-x-1 top-1 pt-24 -mt-24 opacity-0 group-hover:opacity-100 duration-500 transition-all"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              class="w-4 h-4 stroke-blue"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-              />
-            </svg>
-          </a>
-          $texte
-        </div>
-      </div>
-      <div>$code</div>
-    </div>
-HTML;
+        $t .= $header;
     }
+
+    $s = [];
+    foreach ($sections as $i => $section) {
+        $temp = [];
+        $temp['id'] = 'section-'.$i;
+        $temp['text'] = implode('', $section['text']).\PHP_EOL;
+        if ($section['code'] ?? null) {
+            $temp['code'] = '';
+            $temp['code'] .= '```php'.\PHP_EOL;
+            $temp['code'] .= implode('', $section['code']).\PHP_EOL;
+            $temp['code'] .= '```'.\PHP_EOL;
+        }
+        $s[] = $temp;
+    }
+
+    $t .= 'sections: '.PHP_EOL;
+    foreach (explode(PHP_EOL, Yaml::dump($s, 2, 2)) as $line) {
+        $t .= '  ' . $line . PHP_EOL;
+    }
+
+    $t .= '---';
 
     return $t;
 };
